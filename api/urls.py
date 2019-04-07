f8d4b92bf37f768bc794c8332614b342bd566893
Ramber Ri <ramberri@Rambers-Mac.local>
2019-04-07 06:44:08 -0700

    Implement Login & Signup

---
+++ api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from .views import LoginView, SignupView
+
+urlpatterns = [
+	path('login/', LoginView.as_view(), name="login"),
+	path('signup/', SignupView.as_view(), name="signup")
+]
